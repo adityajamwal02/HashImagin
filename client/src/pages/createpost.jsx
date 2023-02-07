4a8bee58c0a454954ad7f8f2539ae3cdbd774a35
riyakansal2901 <riyakansal2901@gmail.com>
2023-02-07 08:38:26 +0530

    setup complete

---
+++ client/src/pages/createpost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const createpost = () => {
+  return (
+    <div>createpost</div>
+  )
+}
+
+export default createpost
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import home from './home';
+import createpost from './createpost';
+
+export{
+  home,
+  createpost
+}
