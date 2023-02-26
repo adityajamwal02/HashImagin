608955488e57dbd6ff10170b500a1ccf755302ac
swatiAg-31 <79588315+swatiAg-31@users.noreply.github.com>
2023-02-26 13:44:12 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,32 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
 import { Card, FormField, Loader } from '../components';
-
 const RenderCards = ({ data, title }) => {
-  if (data?.length > 0) {
+    if (data?.length > 0) {
+      return (
+        data.map((post) => <Card key={post._id} {...post} />)
+      );
+    }
     return (
-      data.map((post) => <Card key={post._id} {...post} />)
-    );
-  }
-
-  return (
-    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
-  );
-};
-
+        <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+      );
+    };
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-  const [allPosts, setAllPosts] = useState(null);
-
-  const [searchText, setSearchText] = useState('');
-  const [searchTimeout, setSearchTimeout] = useState(null);
+   const [loading,setLoading] = useState(false);
+   const [allPosts, setAllPosts] = useState(null);
+   const [searchText, setSearchText] = useState('');
+   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
-
-  const fetchPosts = async () => {
+   const fetchPosts = async () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -47,7 +42,6 @@ const Home = () => {
   useEffect(() => {
     fetchPosts();
   }, []);
-
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
     setSearchText(e.target.value);
@@ -60,13 +54,12 @@ const Home = () => {
     );
   };
 
-  return (
+   return (
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">HashImagin Exhibit</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[600px]">Browse Through Collection of AI Generated Images Powered By OpenAI Codex</p>
       </div>
-
       <div className="mt-16">
         <FormField
           labelName="Search posts"
@@ -77,21 +70,18 @@ const Home = () => {
           handleChange={handleSearchChange}
         />
       </div>
-
-      <div className="mt-10">
+      <div className = "mt-10">
         {loading ? (
-          <div className="flex justify-center items-center">
-            <Loader />
-          </div>
-        ) : (
-          <>
-            {searchText && (
-              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                <div className="flex justify-center items-center">
+                <Loader />
+                 </div>
+            ) : (
+                <>
+                {searchText && (<h2 className="font-medium text-[#666e75] text-xl mb-3">
                 Showing Resuls for <span className="text-[#222328]">{searchText}</span>:
-              </h2>
-            )}
-            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-              {searchText ? (
+                </h2>)}
+                <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+                {searchText ? (
                 <RenderCards
                   data={searchedResults}
                   title="No Search Results Found"
@@ -102,12 +92,13 @@ const Home = () => {
                   title="No Posts Yet"
                 />
               )}
-            </div>
-          </>
-        )}
+                </div>
+                </>
+            )
+        }
       </div>
-    </section>
-  );
+      </section> 
+      );
 };
 
-export default Home;
+export default Home
